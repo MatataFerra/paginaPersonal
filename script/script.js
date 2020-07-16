676ecc71ce5f3e04ff140661c9196f86cfc6c3b1
Matata <matias.agf@gmail.com>
2020-07-16 01:16:25 -0300

    agregado link por JS y smooth scroll

---
+++ script/script.js
@@ -0,0 +1,12 @@
+function crearLink (id, link) {
+    let linkElement = document.getElementById(id);
+    linkElement.setAttribute("href", link);
+    linkElement.setAttribute('target', "_blank");
+}
+
+crearLink('linkedin', "https://www.linkedin.com/posts/matiasgf_orgullo-logro-rrhh-activity-6643160459614699520-wYCz");
+
+
+
+
+
