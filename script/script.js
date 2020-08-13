1b1aa5b395c8a41d489fe5ffa6efc2bdeb0ae4ef
Matata <matias.agf@gmail.com>
2020-08-12 21:11:10 -0300

    actualizando proyecto

--- script/script.js
+++ script/script.js
@@ -5,8 +5,7 @@ function crearLink (id, link) {
 }
 
 crearLink('linkedin', "https://www.linkedin.com/posts/matiasgf_orgullo-logro-rrhh-activity-6643160459614699520-wYCz");
-
-
+crearLink('inFoto', 'https://www.linkedin.com/posts/matiasgf_selecciaejn-vacacionesenlaescuela-activity-6587750401183932416-lhTI')
 
 
 
